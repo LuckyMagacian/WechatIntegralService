a367c500c95386ef7791e7f740e997ae24bac8b6
yangyuanjian <yangyuanjian@188lanxi.com>
2017-01-09 08:55:55 +0800

    不知道改了什麽

--- src/main/java/com/lanxi/WechatIntegralService/service/IntegralGameServiceImpl.java
+++ src/main/java/com/lanxi/WechatIntegralService/service/IntegralGameServiceImpl.java
@@ -2,11 +2,19 @@ package com.lanxi.WechatIntegralService.service;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class IntegralGamerServiceImpl implements IntegralGameService {
+import com.lanxi.WechatIntegralService.util.AppException;
+
+public class IntegralGameServiceImpl implements IntegralGameService {
 
 	@Override
 	public void startGame(HttpServletRequest req) {
-		// TODO Auto-generated method stub
+		try{
+			req.setCharacterEncoding("utf-8");
+			String gameId=req.getParameter("gameId");
+			String token =req.getParameter("token");
+		}catch (Exception e) {
+			throw new AppException("游戏开始异常",e);
+		}
 		
 	}
 
