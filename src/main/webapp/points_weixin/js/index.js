498f228925e1cd4a12cc5c71c8483f63c079e667
yangyuanjian <yangyuanjian@188lanxi.com>
2017-01-16 17:23:51 +0800

    网页增加游戏

--- src/main/webapp/points_weixin/js/index.js
+++ src/main/webapp/points_weixin/js/index.js
@@ -1,11 +1,11 @@
 $(function(){
 	paramsArr = getParam(); //获取url参数数组
-	//getInfo();//获取信息
+	getInfo();//获取信息
 });
 
 function getInfo(){
-	var uri='../intoJf.do';
-	ajaxPost(uri,{},function(jsonStr){
+	var uri='../getInfoByToken.do';
+	ajaxPost(uri,{'token':paramsArr.token},function(jsonStr){
 		var name=jsonStr.name,
 			integralValue=jsonStr.integralValue,
 			headimgUrl=jsonStr.headimgUrl,
