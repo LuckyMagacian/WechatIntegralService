939021bd67797a9ee4c6e0ea1695b3797789d493
zlmifan <zlmifan@sina.cn>
2017-01-23 16:22:37 +0800

    一些修正

--- src/main/webapp/points_weixin/js/turntable.js
+++ src/main/webapp/points_weixin/js/turntable.js
@@ -114,7 +114,7 @@ function getDialog(jsonStr) {
 				msg = "三等奖";
 				break;
 		}
-		retMsg=jsonStr.retMsg+"积分下发至账户/卡券请至卡券包查看";
+		retMsg=jsonStr.retMsg+"(注:积分下发至账户/卡券请至卡券包查看)";
 	} else {
 			msg="谢谢参与";
 			retMsg=jsonStr.retMsg;
